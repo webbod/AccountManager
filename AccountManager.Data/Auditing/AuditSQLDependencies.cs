10f9b043c3c53bc944df77d93579ba3e8c1d9705
Graeme Mulvaney <web.bod@gmail.com>
2020-09-23 13:24:01 +0100

    Added Auditor for sql dependencies

---
+++ AccountManager.Data/Auditing/AuditSQLDependencies.cs
@@ -0,0 +1,45 @@
+﻿using AccountManager.Interfaces.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AccountManager.Data.Auditing
+{
+    public class AuditSQLDependencies
+    {
+        public struct SQLAudit
+        {
+            public string Name { get; set; }
+            public string SQLElement { get; set; }
+            public DatabaseDependency.Element SQLElementType { get; set; }
+            public DatabaseDependency.MaintainenceRisk Risk {get; set;}
+        }
+
+        public static List<SQLAudit> FindDependentMethods()
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            var methods = assembly.GetTypes()
+                      .SelectMany(t => t.GetMethods())
+                      .Where(m => m.GetCustomAttributes(typeof(DatabaseDependency), false).Length > 0)
+                      .ToList();
+
+            var output = new List<SQLAudit>();
+
+            foreach(var method in methods)
+            {
+                var customAttribute = (DatabaseDependency)method.GetCustomAttributes(typeof(DatabaseDependency), false).FirstOrDefault();
+                output.Add(new SQLAudit { 
+                    Name = method.ReflectedType.FullName, 
+                    SQLElement = customAttribute.Name, 
+                    SQLElementType = customAttribute.Dependency, 
+                    Risk = customAttribute.Risk 
+                });
+            }
+
+            return output;
+        }
+    }
+}
