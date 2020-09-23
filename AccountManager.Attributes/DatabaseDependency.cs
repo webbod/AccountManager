545bd0fff32a5ce7428292de8040f07cdf0fe8d7
Graeme Mulvaney <web.bod@gmail.com>
2020-09-23 09:34:54 +0100

    DatabaseDependency attribute to track risky sql code

---
+++ AccountManager.Attributes/DatabaseDependency.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountManager.Interfaces.Attributes
+{
+    /// <summary>
+    /// Allows you to mark-up and assess dependencies on the database structure as part of a code review
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor, Inherited = false, AllowMultiple = true)]
+    public sealed class DatabaseDependency : Attribute
+    {
+        public enum Element
+        {
+            Table,
+            StoredProcedure,
+            Function,
+            Column
+        }
+
+        [Flags]
+        public enum MaintainenceRisk
+        {
+            NothingMuch,
+            ParameterDrift,
+            RawSQLStatement,
+            ComplexLogic,
+            CrossTableDependency,
+            MissingTransaction
+        }
+
+        public string Name
+        {
+            get; private set;
+        }
+        
+        public Element Dependency
+        {
+            get; private set;
+        }
+
+        public MaintainenceRisk Risk 
+        {
+            get; private set;
+        }
+
+        public string Notes
+        {
+            get; private set;
+        }
+
+        // This is a positional argument
+        public DatabaseDependency(string name, Element dependency, MaintainenceRisk risk = MaintainenceRisk.NothingMuch, string notes = null)
+        {
+            Name = name;
+            Dependency = dependency;
+            Risk = risk;
+            Notes = notes;
+        }
+    }
+}
---
+++ AccountManager.Attributes/README.md
@@ -0,0 +1,4 @@
+﻿### DatabaseDependency
+Use this attribute to mark-up code as part of a review to highlight dependencies to database objects and assess the risk that the code pose if the database changes.
+
+You should use it to call attention to any raw SQL expressions as these can be particularly risky.
