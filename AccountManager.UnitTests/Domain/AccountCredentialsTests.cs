0d3e5188f8f02972ea8e7ab0081893bd7e6dae0f
Graeme Mulvaney <web.bod@gmail.com>
2020-09-23 09:51:33 +0100

    AccountCredentials tests

---
+++ AccountManager.UnitTests/Domain/AccountCredentialsTests.cs
@@ -0,0 +1,30 @@
+using AccountManager.Domain.Models;
+using System;
+using Xunit;
+
+namespace AccountManager.UnitTests.Credentials
+{
+    public class AccountCredentialsTests
+    {
+        [Theory]
+        [InlineData("", null, true)]
+        [InlineData("e@ma.il", "", true)]
+        [InlineData("", "password", true)]
+        [InlineData("e@ma.il", "password", false)]
+        public void CredentialsAreValidIfNothingIsNullOrEmpty(string emailAddress, string plainTextPassword, bool shouldThrowException)
+        {
+            var credentials = new AccountCredentials { EmailAddress = emailAddress, PlainTextPassword = plainTextPassword };
+
+            try
+            {
+                var actual = AccountCredentials.TryValidate(credentials);
+                Assert.True(actual);
+            }
+            catch(ArgumentNullException)
+            {
+                Assert.True(shouldThrowException);
+            }
+            
+        }
+    }
+}
