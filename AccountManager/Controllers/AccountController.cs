f94bc274f2569377cbf7c8c570ed58e00a04dabb
Graeme Mulvaney <web.bod@gmail.com>
2020-09-23 12:56:18 +0100

    Account controller, CredentialsViewModel and the associated views

---
+++ AccountManager/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using AccountManager.Data;
+using AccountManager.Domain.Models;
+using AccountManager.Interfaces.Accounts;
+using AccountManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+namespace AccountManager.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private IAccountFinder _FinderService;
+        private IAccountUpdater _UpdaterService;
+
+        public AccountController(IAccountFinder finderService, IAccountUpdater updaterService, IOptions<SqlDataStoreConfigurationSettings> options)
+        {
+            _FinderService = finderService;
+            _FinderService.Initalise(options.Value);
+            _UpdaterService = updaterService;
+            _UpdaterService.Initalise(options.Value);
+        }
+        public IActionResult Index()
+        { 
+            return View(new CredentialsViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CredentialsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _UpdaterService.Update(model);
+                model.WasSaved = true;
+                
+                // we don't need this anymore
+                model.PlainTextPassword = string.Empty;
+
+                return View(model);
+            }
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public JsonResult CheckIfEmailIsNotInUse(string emailAddress)
+        {
+            bool isAvailable = false;
+            try
+            {
+                _FinderService.Find(emailAddress);
+            }
+            catch (KeyNotFoundException)
+            {
+                isAvailable = true;
+            }
+
+            return Json(isAvailable);
+        }
+    }
+}
+
---
+++ AccountManager/Models/CredentialsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using AccountManager.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AccountManager.Models
+{
+    public class CredentialsViewModel : AccountCredentials
+    {
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Email address")]
+        [StringLength(maximumLength: 200, ErrorMessage = "This email address is too long (max: 200 characters)")]
+        [EmailAddress(ErrorMessage = "Please use a valid email address")]
+        [Remote(action: "CheckIfEmailIsNotInUse", controller:"Account", ErrorMessage = "This email address is already in use", HttpMethod = "Post")]
+        public override string EmailAddress { get => base.EmailAddress; set => base.EmailAddress = value; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Password")]
+        [PasswordPropertyText(true)]
+        [StringLength(maximumLength: 32, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 32 characters in length")]
+        [RegularExpression(pattern: @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^\da-zA-Z]).*$", ErrorMessage = "Password must contain at least one: number, upper-case letter, lower-case letter and a symbol")]
+        public override string PlainTextPassword { get => base.PlainTextPassword; set => base.PlainTextPassword = value; }
+
+        public bool WasSaved { get; set; }
+    }
+}
