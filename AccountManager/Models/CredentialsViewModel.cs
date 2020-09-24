9d5838f3f0879fa99f2bc134d870e8fd16fa994c
Graeme Mulvaney <web.bod@gmail.com>
2020-09-24 09:25:15 +0100

    Move placeholders back on to the view model

--- AccountManager/Models/CredentialsViewModel.cs
+++ AccountManager/Models/CredentialsViewModel.cs
@@ -8,14 +8,14 @@ namespace AccountManager.Models
     public class CredentialsViewModel : AccountCredentials
     {
         [Required(AllowEmptyStrings = false)]
-        [Display(Name = "Email address")]
+        [Display(Name = "Email address", Prompt = "Email address")]
         [StringLength(maximumLength: 200, ErrorMessage = "This email address is too long (max: 200 characters)")]
         [EmailAddress(ErrorMessage = "Please use a valid email address")]
         [Remote(action: "CheckIfEmailIsNotInUse", controller:"Account", ErrorMessage = "This email address is in use", HttpMethod = "Post")]
         public override string EmailAddress { get => base.EmailAddress; set => base.EmailAddress = value; }
 
         [Required(AllowEmptyStrings = false)]
-        [Display(Name = "Password")]
+        [Display(Name = "Password", Prompt = "Password")]
         [PasswordPropertyText(true)]
         [StringLength(maximumLength: 32, MinimumLength = 8, ErrorMessage = "Password must be 8-32 characters")]
         [RegularExpression(pattern: @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^\da-zA-Z]).*$", ErrorMessage = "Password needs letters, numbers and symbols")]
