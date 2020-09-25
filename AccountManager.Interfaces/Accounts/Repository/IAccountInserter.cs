ab71fe93ebbefb999f29d417a7886801f83591c3
Graeme Mulvaney <web.bod@gmail.com>
2020-09-25 08:56:40 +0100

    The Insert behaviour needs to be more explicit

---
+++ AccountManager.Interfaces/Accounts/Repository/IAccountInserter.cs
@@ -0,0 +1,12 @@
+﻿using AccountManager.Interfaces.DataStore;
+using System.ComponentModel;
+
+namespace AccountManager.Interfaces.Accounts.Repository
+{
+    public interface IAccountInserter
+    {
+        void Initalise(ISetOfConfigrationSettings options);
+
+        IAccount Insert(IAccountCredentials credentials);
+    }
+}
