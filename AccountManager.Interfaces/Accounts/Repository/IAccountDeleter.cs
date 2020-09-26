f6849e14f366ca64b80c900bb8191282fd1b7c0d
Graeme Mulvaney <web.bod@gmail.com>
2020-09-26 22:53:49 +0100

    Update IAccountDeleter.cs
    Implemented interface correctly

--- AccountManager.Interfaces/Accounts/Repository/IAccountDeleter.cs
+++ AccountManager.Interfaces/Accounts/Repository/IAccountDeleter.cs
@@ -5,7 +5,7 @@ namespace AccountManager.Interfaces.Accounts.Repository
     /// <summary>
     /// Describes a mechanism for Deleting Accounts
     /// </summary>
-    public interface IAccountDeleter : IDeleter<string>
+    public interface IAccountDeleter : IDeleter<IAccountCredentials>
     {
     }
 }
